ccaadf8dac2a80ab351a19edfdbbc1e82f833bbe
liupeng <liupeng.0@outlook.com>
2019-11-19 17:18:40 +0800

    显示Cord组成的ListView V 0.0.5

---
+++ lib/Card.dart
@@ -0,0 +1,60 @@
+/**
+ * ListTile
+ */
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "ListTile",
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("ListTile"),
+        ),
+        body: new MyCard(),
+      ),
+    );
+  }
+}
+
+class MyCard extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyCardState();
+  }
+}
+
+class MyCardState extends State<MyCard> {
+  var _throwShotAway = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        ListTile(
+          title: Text('${123.45}', style: TextStyle(fontWeight: FontWeight.w500),),
+          subtitle: Text('${DateTime.now()}' + '曾小晖：小灰灰：13885458655'),
+          leading: Icon(Icons.adjust,color: Colors.lightBlue,),
+          // Icon 图标
+        ),
+        Divider(),// 分割线
+        ListTile(
+          title: Text('${DateTime.now()}',style: TextStyle(fontWeight: FontWeight.w500),),
+          subtitle: Text('小灰灰：13885458655'),
+          leading: Icon(Icons.adjust,color: Colors.deepOrange),
+        ),
+        Divider(), // 分割线
+        ListTile(
+          title: Text('${DateTime.now()}',style: TextStyle(fontWeight: FontWeight.w500),),
+          subtitle: Text('小灰灰：小灰灰：13885458655'),
+          leading: Icon(Icons.adjust,color: Colors.lightBlue),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/ShowList.dart
@@ -0,0 +1,76 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: DayList(date: "2019-11-18",),
+    );
+  }
+}
+
+class DayList extends StatelessWidget {
+
+  String date;
+
+  DayList({this.date});
+
+  double a;
+  double b;
+  double c;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${this.date}消费清单"),
+      ),
+      body: Center(
+        child:CustomScrollView(
+          shrinkWrap: true,
+          slivers: <Widget>[
+            SliverPadding(
+              padding: const EdgeInsets.all(20.0),
+              sliver: SliverList(
+                delegate: SliverChildListDelegate(
+                  <Widget>[
+                    const Text('I\'m dedicating every day to you'),
+                    const Text('Domestic life was never quite my style'),
+                    const Text('When you smile, you knock me out, I fall apart'),
+                    const Text('And I thought I was so smart'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+}
+
+/*
+ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(20.0),
+              children: <Widget>[
+                const Text('I\'m dedicating every day to you'),
+                const Text('Domestic life was never quite my style'),
+                const Text('When you smile, you knock me out, I fall apart'),
+                const Text('And I thought I was so smart'),
+              ],
+            )
+            */
